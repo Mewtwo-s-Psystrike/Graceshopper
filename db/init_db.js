5eab7251cb9c07769317ecafc9f98c10d744c4c3
Andy Nguyen <andyng1997@outlook.com>
2023-01-07 10:28:39 -0500

    started to create initial product

--- db/init_db.js
+++ db/init_db.js
@@ -7,7 +7,7 @@ const {
 async function buildTables() {
   try {
     client.connect();
-
+isAdmin BOOLEAN DEFAULT false
     // drop tables in correct order
 
     // build tables in correct order
@@ -16,15 +16,28 @@ async function buildTables() {
   }
 }
 
-async function populateInitialData() {
+//title, year, make, model, description, color, price, inventory
+
+async function createInitialProducts() {
   try {
-    // create useful starting data by leveraging your
-    // Model.method() adapters to seed your db, for example:
-    // const user1 = await User.createUser({ ...user info goes here... })
+      console.log('Creating new products.......');
+      const productsToCreate = [
+        {
+          title: "2023 Tesla Model S",
+          year: "2023",
+          make: "Tesla",
+          model: "Model S Plaid",
+          description: "Maintain 1,000+ horsepower all the way to 200 mph with Tri-Motor All-Wheel Drive, featuring torque vectoring and three independent carbon-sleeved rotors. With a 0-60mph time of 1.99s, this car will really throw your head back into the seat! At 1,020hp, this makes it one of the fastests cars in production. Even with all that power, you can still enjoy a nice 396miles of range.",
+          color: "Blue",
+          price: "$127,590",
+          inventory: "Available",
+      }]
   } catch (error) {
     throw error;
   }
 }
+
+
 
 buildTables()
   .then(populateInitialData)
