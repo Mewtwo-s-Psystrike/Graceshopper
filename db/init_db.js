9434e87dffb28b5f0bbeff398efe8073fda90918
nicko66 <109779785+nicko66@users.noreply.github.com>
2023-01-07 09:49:11 -0600

    created users and made admins

--- db/init_db.js
+++ db/init_db.js
@@ -16,6 +16,27 @@ async function buildTables() {
   }
 }
 
+async function createInitialUsers() {
+  console.log("Creating users...")
+  try {
+    const usersToCreate = [
+      { username: "andy", password: "andy1234", isAdmin: true },
+      { username: "marcus", password: "marcus1234", isAdmin: true },
+      { username: "jaycee", password: "jaycee1234", isAdmin: true },
+      { username: "nick", password: "nick1234", isAdmin: true },
+    ]
+    const users = await Promise.all(usersToCreate.map(createUser))
+
+    console.log("Users created:")
+    console.log(users)
+    console.log("Finished creating users!")
+  } catch (error) {
+    console.error("Error creating users!")
+    throw error
+  }
+}
+
+
 async function populateInitialData() {
   try {
     // create useful starting data by leveraging your
