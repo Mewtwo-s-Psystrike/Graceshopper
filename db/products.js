b0dac7d8da6224b00b8d495e15802a6340adc841
Andy Nguyen <andyng1997@outlook.com>
2023-01-12 21:06:27 -0500

    added template for products db

---
+++ db/products.js
@@ -0,0 +1,12 @@
+const client = require("./client");
+
+/*reference activity in fitness trackr backend
+
+Write Database functions, we need these functions from activities but modified into graceshopper instead.
+
+async function getAllProducts(), getProductById(id), getProductByTitle(title), createProduct({ title, imageurl, year, make, model, description, color, price, inventory}),
+updateProducts({id, ...fields}), deleteProduct(productId).
+
+*/
+
+
