2dd5bde7e4f0bb6109b3419cde934895edccef6e
Jaycee Xu <jaycee4845@gmail.com>
2023-01-17 18:17:43 -0600

    removed requireUser function from the db/user.js file

--- db/user.js
+++ db/user.js
@@ -58,20 +58,8 @@ async function getUserById(userId) {
   }
 }
 
-function requireUser(req, res, next) {    
-  if (!req.user) {
-    res.status(401).send({
-    error: "User not logged in",
-    name: "MissingUserError", 
-    message: "You must be logged in to perform this action"});
-    }
-    next();  
-} 
-
-
 module.exports = {
   createUser,
   getUser,
   getUserById,
-  requireUser,
 }
