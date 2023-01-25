6a1a0bd764052752c41b5b4e7ee3563d33ac98fb
Marcus Mills Jr <marcus.mills97@yahoo.com>
2023-01-24 19:24:51 -0500

    feat: added styling to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import home from "../images/home.jpg";
-import tesla from "../images/tesla.png";
 
 
 const Home = () => {
@@ -9,9 +8,7 @@ const Home = () => {
         <>
         <img src={home} alt="Concept" className="home"/>
         <body className="oneHunVh">
-        <Link to="/products" className="modelshome">MODELS</Link>
         <i class="bi bi-arrow-bar-down"></i>
-        <img src={tesla} alt="Tesla Logo" height={100} className="tesla"/>
         </body>
         </>
         
